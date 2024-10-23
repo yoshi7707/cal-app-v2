7ead9e41b0e9922555e5fbaa17108ce242b04640
yoshi7707 <ytaguchi@gmail.com>
2024-10-23 17:24:54 +0900

    before commit

--- pages/index-copy.js
+++ pages/index-copy.js
@@ -186,74 +186,6 @@ const MyCalendar = () => {
   };
 
 //drug & copy proccess ==================================================
-  
-  // const eventsForCopy = [
-  //   {
-  //     id: 0,
-  //     title: 'Board meeting',
-  //     start: new Date(2018, 0, 29, 9, 0, 0),
-  //     end: new Date(2018, 0, 29, 13, 0, 0),
-  //     resourceId: [1, 2],
-  //   },
-  //   {
-  //     id: 1,
-  //     title: 'MS training',
-  //     start: new Date(2018, 0, 29, 14, 0, 0),
-  //     end: new Date(2018, 0, 29, 16, 30, 0),
-  //     resourceId: 2,
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'Team lead meeting',
-  //     start: new Date(2018, 0, 29, 8, 30, 0),
-  //     end: new Date(2018, 0, 29, 12, 30, 0),
-  //     resourceId: 3,
-  //   },
-  //   {
-  //     id: 10,
-  //     title: 'Board meeting',
-  //     start: new Date(2018, 0, 30, 23, 0, 0),
-  //     end: new Date(2018, 0, 30, 23, 59, 0),
-  //     resourceId: 1,
-  //   },
-  //   {
-  //     id: 11,
-  //     title: 'Birthday Party',
-  //     start: new Date(2018, 0, 30, 7, 0, 0),
-  //     end: new Date(2018, 0, 30, 10, 30, 0),
-  //     resourceId: 4,
-  //   },
-  //   {
-  //     id: 12,
-  //     title: 'Board meeting',
-  //     start: new Date(2018, 0, 29, 23, 59, 0),
-  //     end: new Date(2018, 0, 30, 13, 0, 0),
-  //     resourceId: 1,
-  //   },
-  //   {
-  //     id: 13,
-  //     title: 'Board meeting',
-  //     start: new Date(2018, 0, 29, 23, 50, 0),
-  //     end: new Date(2018, 0, 30, 13, 0, 0),
-  //     resourceId: 2,
-  //   },
-  //   {
-  //     id: 14,
-  //     title: 'Board meeting',
-  //     start: new Date(2018, 0, 29, 23, 40, 0),
-  //     end: new Date(2018, 0, 30, 13, 0, 0),
-  //     resourceId: 4,
-  //   },
-  // ]
-  
-  // const resourceMap = [
-  //   { resourceId: 1, resourceTitle: 'Board room' },
-  //   { resourceId: 2, resourceTitle: 'Training room' },
-  //   { resourceId: 3, resourceTitle: 'Meeting room 1' },
-  //   { resourceId: 4, resourceTitle: 'Meeting room 2' },
-  // ]
-
-//drug & copy proccess ==================================================
 
 
   const [selectedEvent, setSelectedEvent] = useState(null);
@@ -723,7 +655,6 @@ const MyCalendar = () => {
     [fetchEvents, setMyEvents]
   );
 
-  
   const handleEventPaste = ({ event, e }) => {
     // Handle event paste logic here
     console.log('Event pasted:', event);
